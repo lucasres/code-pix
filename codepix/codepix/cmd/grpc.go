e9ee2cc9e6c8d75269acc62df79617f21e865788
mandala21 <lucasresone@gmail.com>
2021-02-04 14:01:38 -0300

    FEATURE add comando para iniciar servidor grpc via kafka

---
+++ codepix/codepix/cmd/grpc.go
@@ -0,0 +1,51 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"os"
+	"github.com/spf13/cobra"
+	"github.com/lucasres/code-pix/infrastructure/db"
+	"github.com/jinzhu/gorm"
+	"github.com/lucasres/code-pix/application/grpc"
+)
+
+var portInit int
+var database *gorm.DB
+// grpcCmd represents the grpc command
+var grpcCmd = &cobra.Command{
+	Use:   "grpc",
+	Short: "Inicia o servidor de grpc",
+	Run: func(cmd *cobra.Command, args []string) {
+		database = db.CreateDB(os.Getenv("ENV"))
+		grpc.StartGrpcServer(database, portInit)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(grpcCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// grpcCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// grpcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	grpcCmd.Flags().IntVarP(&portInit, "port", "p", 55051, "gRPC server port")
+}
